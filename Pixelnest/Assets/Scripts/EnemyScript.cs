83210011b7a221a8221968b395cb0fe0cb94b44e
panxu <panxu@baidu.com>
2014-01-26 18:08:23 +0800

    完成滚动 并且能够产生敌人

--- Pixelnest/Assets/Scripts/EnemyScript.cs
+++ Pixelnest/Assets/Scripts/EnemyScript.cs
@@ -3,24 +3,60 @@ using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
 
+	private bool hasSpawn;
 	private WeaponScript[] weapons;
+	private MoveScript moveScript;
 
 	void Awake() {
 		weapons = GetComponentsInChildren<WeaponScript>();
+
+		moveScript = GetComponent<MoveScript>();
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		hasSpawn = false;
+
+		collider2D.enabled = false;
+
+		moveScript.enabled = false;
+
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = false;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		foreach (WeaponScript weapon in weapons) {
-			if (weapon != null && weapon.CanAttack) {
-				weapon.Attack(true);
+		if (!hasSpawn) {
+			if (renderer.IsVisibleFrom(Camera.main)) {
+				Spawn();
 			}
+
+		} else {
+
+			foreach (WeaponScript weapon in weapons) {
+				if (weapon != null && weapon.CanAttack) {
+					weapon.Attack(true);
+				}
+			}
+
+			if (!renderer.IsVisibleFrom(Camera.main)) {
+				Destroy(gameObject);
+			}
+
+		}
+	}
+
+	private void Spawn() {
+		hasSpawn = true;
+
+		collider2D.enabled = true;
+
+		moveScript.enabled = true;
+		foreach (WeaponScript weapon in weapons) {
+			weapon.enabled = false;
 		}
 
 	}
--- Pixelnest/Assets/Scripts/PlayerScript.cs
+++ Pixelnest/Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class PlayerScript : MonoBehaviour {
 	
 	/// 飞船的速度
@@ -35,6 +36,25 @@ public class PlayerScript : MonoBehaviour {
 
 			weapon.Attack(false);
 		}
+
+		var dist = (transform.position - Camera.main.transform.position).z;
+
+		var leftBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)).x;
+
+		var rightBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(1, 0, dist)).x;
+
+		var topBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 0, dist)).y;
+
+		var bottomBorder = Camera.main.ViewportToWorldPoint(
+			new Vector3(0, 1, dist)).y;
+
+		transform.position = new Vector3(
+			Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+			Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+			transform.position.z);
 
 	}
 
