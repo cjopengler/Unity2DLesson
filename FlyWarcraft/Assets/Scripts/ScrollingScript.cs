d713bcf89e4a64ba8fa98591982a38cfad98a34c
panxu <panxu@baidu.com>
2014-03-01 16:47:30 +0800

    增加关卡和滚屏的处理

---
+++ FlyWarcraft/Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * 视差滚动脚本
+ * 
+ * */
+public class ScrollingScript : MonoBehaviour {
+
+	//滚动的速度
+	public Vector2 speed = new Vector2(2, 2);
+
+	//滚动的方向
+	public Vector2 direction = new Vector2(0, -1);
+
+	public bool isLinkedToCamera = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
